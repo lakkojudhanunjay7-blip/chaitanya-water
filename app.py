789c49494e3b263c25e6827e94b72f8ed1e9deeb
lakkojudhanunjay7-blip <lakkojudhanunjay7@gmail.com>
2026-01-13 11:10:16 +0530

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -273,4 +273,5 @@ def static_files(filename):
 if __name__ == "__main__":
     if not os.path.exists(DB_PATH):
         open(DB_PATH, "a").close()
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    #app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
